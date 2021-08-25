e074a7e911b61b2f14b40b22936a08c845de796c
weber <benjamin.weber@it.uu.se>
2021-08-25 13:09:41 +0200

    Began work on test for Hermite derivatives, have added a new header file for testing finite element derivatives in general.

---
+++ tests/fe/derivatives.h
@@ -0,0 +1,27 @@
+
+
+// Plot derivatives of finite elements at corner nodes of element
+
+#ifndef DERIVATIVE_TEST_HEADER
+#define DERIVATIVE_TEST_HEADER
+
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/dofs/dof_accessor.h>
+
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/mapping_q1.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_iterator.h>
+
+#include <deal.II/lac/vector.h>
+
+#include <string>
+#include <vector>
+
+#include "../tests.h"
+
+
+#endif
