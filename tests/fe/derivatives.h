b28700f0502ed6857a3c91fbc927f450c0541ebd
weber <benjamin.weber@it.uu.se>
2021-09-09 15:33:33 +0200

    Created test for Hermite derivative values at nodes, saved correct results in an output file

--- tests/fe/derivatives.h
+++ tests/fe/derivatives.h
@@ -36,7 +36,7 @@ char fname[50];
 
 template <int dim>
 inline void
-plot_function_derivatives(Mapping<dim> &      mapping,
+plot_function_derivatives(MappingQ<dim> &      mapping,
                           FiniteElement<dim> &finel,
                           const char *        name)
 {
@@ -47,20 +47,20 @@ plot_function_derivatives(Mapping<dim> &      mapping,
     typename DoFHandler<dim>::cell_iterator c = dof.begin();
     
     const unsigned int sz = finel.n_dofs_per_cell();
-    const unsigned int degree = static_cast<int>(std::pow(sz, 1./dim)) - 1;
-    const unsigned int max_test_deriv = std::min(((dim == 1) ? 4 : (dim == 2) ? 2 : 1), (degree + 1) / 2);
+    const unsigned int degree = finel.tensor_degree();
+    const unsigned int max_test_deriv = std::min(((dim == 1) ? 3 : 1), static_cast<int>(degree - 1) / 2);
     const unsigned int div = 1;
     
     QTrapez<dim> q;
     FEValues<dim> fe(mapping,
                      finel,
                      q,
-                     UpdateFlags(update_values | update_gradients | update_hessians | update_3rd_derivatives | update_4th_derivatives ));
+                     UpdateFlags(update_values | update_gradients | update_hessians | update_3rd_derivatives ));
                      
     sprintf(fname, "Cell-%dd-%s", dim, name);
     deallog.push(fname);
     
-    dof.reinit(c);
+    fe.reinit(c);
     
     unsigned int k = 0;
     
@@ -76,7 +76,7 @@ plot_function_derivatives(Mapping<dim> &      mapping,
                 {
                     for (unsigned int dy = 0; dy <= ((dim > 1) ? max_test_deriv : 0); ++dy)
                     {
-                        for unsigned int dx = 0; dx <= max_test_deriv; ++dx)
+                        for (unsigned int dx = 0; dx <= max_test_deriv; ++dx)
                         {
                             deallog << q.point(k) << " " << dx;
                             if (dim > 1)
@@ -97,15 +97,9 @@ plot_function_derivatives(Mapping<dim> &      mapping,
                                         deallog << " " << fe.shape_grad(i,k)[entry_1] + 1.;
                                         break;
                                     case 2:
-                                        if (std::max(dx, dy) == 2)     // dz can never be higher than 1, so checking if either dx or dy is 2 is sufficient
-                                            entry_1 = entry_2 = dy / 2;
-                                        else if (dz == 0)
-                                            entry_1 = 0, entry_2 = 1;
-                                        else if (dy == 0)
-                                            entry_1 = 0, entry_2 = 2;
-                                        else
-                                            entry_1 = 1, entry_2 = 2;
-                                        deallog << " " << fe.shape_grad_grad(i,k)[entry_1][entry_2] + 1.;
+                                        entry_1 = (dx == 0) ? 1 : 0;
+                                        entry_2 = 2 * dz - dy * dz + dy;
+                                        deallog << " " << fe.shape_hessian(i,k)[entry_1][entry_2] + 1.;
                                         break;
                                     case 3:
                                         if (dz == 1)
@@ -114,11 +108,7 @@ plot_function_derivatives(Mapping<dim> &      mapping,
                                             entry_1 = entry_2 = entry_3 = dy / 3;
                                         else
                                             entry_1 = 0, entry_2 = dy - 1, entry_3 = 1;
-                                        deallog << " " << fe_shape_3rd_derivative(i,k)[entry_1][entry_2][entry_3] + 1.;
-                                        break;
-                                    case 4:
-                                        entry_1 = entry_2 = (dx == 4) ? 0 : 1; //either dx=4 or dx=dy=2
-                                        deallog << " " << fe.shape_4th_derivative(i,k)[0][0][entry_1][entry_2] + 1.;
+                                        deallog << " " << fe.shape_3rd_derivative(i,k)[entry_1][entry_2][entry_3] + 1.;
                                         break;
                                     default:
                                         ExcMessage("Requested order of derivative is too high for current implementation.");
